7e7abf16581f1fd594a559ef99ccb2354904b753
Marcos Brandan <marcosbrandan681@gmail.com>
2025-11-03 15:34:15 -0300

    Se crea la conexion con la base de datos

---
+++ routes/admin.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+//import { agregarUsuario } from "../controllers/admin.controllers.js";
+
+const routerAdmin = Router();
+
+//routerAdmin.post('/crearUsuario', agregarUsuario);
+
+export default routerAdmin;
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+//import { obtenerUsuarios } from "../controllers/admin.controllers.js";
+
+//Endpoint para obtener los usuarios
+
+const router = Router();
+
+//router.get('/usuarios', obtenerUsuarios);
+
+export default router;
