377e27ce8731e4838d93b6828aca6bd00906fa57
Marcos Brandan <marcosbrandan681@gmail.com>
2025-11-21 18:35:15 -0300

    Se agrega el endpoint para la baja logica de una funcion

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -3,20 +3,14 @@ import { pool } from '../database/db.js';
 const agregarPelicula = async (req, res) => {
 
     try {
-        // 3. Los datos de TEXTO vienen en 'req.body'
         const { nombre, duracion, lanzamiento, descripcion, CATEGORIA_id } = req.body;
 
-        // 4. Los datos del ARCHIVO vienen en 'req.file'
-        //    Si no se sube un archivo, req.file será 'undefined'
         if (!req.file) {
             return res.status(400).json({ message: 'No se subió ningún archivo de imagen' });
         }
 
-        // 5. Esta es la ruta que guardaremos en la BD
         const imagenUrl = req.file.path;
-        // (Esto guardará algo como "uploads/pelicula-123456789.jpg")
-
-        // 6. Insertar en la base de datos
+        
         const [result] = await pool.query(
             'INSERT INTO pelicula (nombre, duracion, lanzamiento, descripcion, imagen, CATEGORIA_id) VALUES (?, ?, ?, ?, ?, ?)',
             [nombre, duracion, lanzamiento, descripcion, imagenUrl, CATEGORIA_id]
@@ -28,7 +22,7 @@ const agregarPelicula = async (req, res) => {
             duracion,
             lanzamiento,
             descripcion,
-            imagen: imagenUrl, // Devolvemos la ruta correcta
+            imagen: imagenUrl, 
             CATEGORIA_id,
         });
 
@@ -40,7 +34,7 @@ const agregarPelicula = async (req, res) => {
 
 const getPeliculas = async (req, res) => {
     try {
-        // Obtenemos todas las películas de la base de datos
+        
         const [rows] = await pool.query('SELECT * FROM pelicula');
         res.json(rows);
     } catch (error) {
@@ -168,20 +162,17 @@ const agregarEntrada = async (req, res) => {
     }
 }
 
-// Endpoint: /crear-funcion-transaccion
+
 const crearFuncionyEntrada = async (req, res) => {
-    console.log("----> DATOS RECIBIDOS DEL FRONTEND:", req.body);
-    // Obtenemos una conexión del pool para poder iniciar la transacción
+    
     const connection = await pool.getConnection();
 
     try {
         const { PELICULA_id, SALA_id, fecha, hora, precio } = req.body;
 
-        // 1. INICIAR TRANSACCIÓN
         // A partir de aquí, nada se guarda "de verdad" hasta que hagamos commit
         await connection.beginTransaction();
 
-        // 2. INSERTAR LA FUNCIÓN
         const [resultFuncion] = await connection.execute(
             'INSERT INTO funcion (fecha, hora, PELICULA_id, SALA_id) VALUES (?, ?, ?, ?)',
             [fecha, hora, PELICULA_id, SALA_id]
@@ -190,25 +181,19 @@ const crearFuncionyEntrada = async (req, res) => {
         // Obtenemos el ID que se acaba de generar
         const funcionIdGenerado = resultFuncion.insertId;
 
-        // 3. INSERTAR EL PRECIO (ENTRADA) USANDO ESE ID
         await connection.execute(
             'INSERT INTO entrada (precio, FUNCION_id) VALUES (?, ?)',
             [precio, funcionIdGenerado]
         );
 
-        // 4. CONFIRMAR CAMBIOS (COMMIT)
-        // Si llegamos aquí, todo salió bien. Guardamos permanentemente.
         await connection.commit();
 
-        // 5. Responder al cliente
         res.json({
             message: 'Éxito total',
             id: funcionIdGenerado
         });
 
     } catch (error) {
-        // 6. SI ALGO FALLA... REVERTIR TODO (ROLLBACK)
-        // Si falla el insert de entrada, se borra el insert de función automáticamente
         await connection.rollback();
         console.error(error);
         res.status(500).json({ message: 'Error en la transacción', error: error.message });
@@ -216,6 +201,21 @@ const crearFuncionyEntrada = async (req, res) => {
     } finally {
         // Siempre liberar la conexión al final
         connection.release();
+    }
+}
+
+const deleteFuncion = async (req, res) => {
+    try {
+        const { id } = req.query;
+        console.log(id)
+        const result = await pool.query('update funcion set estado = ? where id = ?', [0, id]);
+
+        res.status(200).json({
+            msg: 'Funcion dada de baja correctamente'
+        })
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar la funcion' });
     }
 }
 
@@ -231,5 +231,6 @@ export {
     getSalas,
     agregarEntrada,
     crearFuncionyEntrada,
-    getFuncionesForAdmin
+    getFuncionesForAdmin,
+    deleteFuncion
 }
--- routes/admin.js
+++ routes/admin.js
@@ -5,7 +5,7 @@ import { upload } from '../middleware/upload.js';
 // 2. Importa el middleware que revisa si es admin (¡importante!)
 import { authRequired, isAdmin } from '../middleware/validateToken.js';
 // 3. (Asumo que tienes un pool de MySQL)
-import { agregarEntrada, agregarFuncion, agregarPelicula, agregarSala, crearFuncionyEntrada, getCategorias, getFunciones, getFuncionesForAdmin, getPeliculaPorId, getPeliculas, getPeliculasPorCategoria, getSalas } from "../controllers/admin.controllers.js";
+import { agregarEntrada, agregarFuncion, agregarPelicula, agregarSala, crearFuncionyEntrada, deleteFuncion, getCategorias, getFunciones, getFuncionesForAdmin, getPeliculaPorId, getPeliculas, getPeliculasPorCategoria, getSalas } from "../controllers/admin.controllers.js";
 
 const routerAdmin = Router();
 
@@ -27,6 +27,7 @@ routerAdmin.get('/getSalas', getSalas);
 routerAdmin.post('/agregarEntrada', agregarEntrada);
 routerAdmin.post('/agregarFuncionEntrada', crearFuncionyEntrada);
 routerAdmin.get('/getFuncionesForAdmin', getFuncionesForAdmin);
+routerAdmin.put('/eliminarFuncion', deleteFuncion);
 
 
 
